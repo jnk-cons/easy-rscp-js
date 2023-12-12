d5793fbd6527f0d30ebd387b215ee0c04b94673e
Jochen Zink <jochen.zink@jnk-consulting.de>
2023-12-12 20:58:17 +0100

    #1 api/crypt documented

--- src/api/crypt/aes-cipher-factory.ts
+++ src/api/crypt/aes-cipher-factory.ts
@@ -1,5 +1,30 @@
 import {AESCipher} from './aes-cipher';
 
+/**
+ * Factory to create AESCipher instances
+ *
+ * @since 0.5.1
+ *
+ * @see AESCipher
+ *
+ * @public
+ * @interface
+ * @export
+ */
 export interface AesCipherFactory {
+
+    /**
+     * Creates a new instance.
+     *
+     * According to the E3DC requirements, an array of zeros is (unfortunately) used as IV here.
+     *
+     * @returns Newly created instance
+     *
+     * @since 0.5.1
+     *
+     * @see AESCipher
+     *
+     * @public
+     */
     buildCipher(): AESCipher
 }
--- src/api/crypt/aes-cipher.ts
+++ src/api/crypt/aes-cipher.ts
@@ -1,5 +1,37 @@
+/**
+ * Service to encrypt or decrypt a given byte message using AES.
+ *
+ * The service is stateful and designed for E3DC home power plants and follows the algorithm required there.
+ * For example, the last block of the last encrypted message is used as IV for the next one.
+ *
+ * @since 0.5.1
+ *
+ * @interface
+ * @export
+ * @public
+ */
 export interface AESCipher {
+    /**
+     * Encrypts the given message using AES
+     *
+     * @param message Unencrypted message to be encrypted
+     * @returns Encrypted message
+     *
+     * @since 0.5.1
+     *
+     * @public
+     */
     encrypt(message: Buffer): Buffer
 
+    /**
+     * Decrypts the given message using AES
+     *
+     * @param message Encrypted message to be decrypted
+     * @returns Decrypted message
+     *
+     * @since 0.5.1
+     *
+     * @public
+     */
     decrypt(message: Buffer): Buffer
 }
