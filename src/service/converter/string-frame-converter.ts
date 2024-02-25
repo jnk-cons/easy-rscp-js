fb43adb1e0f301044a9ce89ac562322d0565cc6a
Jochen Zink <jochen.zink@jnk-consulting.de>
2024-02-25 21:36:41 +0100

    25 added omit flag to the string-frame-converter.ts

--- src/service/converter/string-frame-converter.ts
+++ src/service/converter/string-frame-converter.ts
@@ -4,8 +4,15 @@ import {Data} from '../../api/frame/data';
 import {DataParser} from '../../api/frame/data-parser';
 import {DataType} from '../../api/frame/DataType';
 import {determineDataTypeName, determineNamespaceName, determineTagName} from '../../api/helper/type-helper';
+import {RSCPTag} from '../../api/frame/tags/RSCPTag';
 
 export class StringFrameConverter implements FrameConverter<string> {
+
+    private TAGS_TO_OMIT = [RSCPTag.AUTHENTICATION_USER, RSCPTag.AUTHENTICATION_PASSWORD, RSCPTag.SET_ENCRYPTION_PASSPHRASE]
+
+    constructor(private omitAuthenticationData: boolean = true) {
+    }
+
     convert(frame: Frame): string {
         let result = 'Timestamp:\t' + frame.timestamp + '\n'
         result += 'WithChecksum:\t' + frame.isChecksumEnabled() + '\n'
@@ -25,9 +32,19 @@ export class StringFrameConverter implements FrameConverter<string> {
             })
         }
         else {
-            result += tabs + 'Value:\t' + toPrint.valueAsString() + ' - ' + toPrint.valueAsHex + '\n\n'
+            result += tabs + 'Value:\t'
+            if (this.shouldMaks(toPrint)) {
+                result += '*** - ***\n\n'
+            }
+            else {
+                result += toPrint.valueAsString() + ' - ' + toPrint.valueAsHex + '\n\n'
+            }
         }
         return result
+    }
+
+    private shouldMaks(toPrint: Data): boolean {
+        return this.omitAuthenticationData && this.TAGS_TO_OMIT.find(value => value == toPrint.tag) != undefined
     }
 
 }
